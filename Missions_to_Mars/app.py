bf361a34e4dafa5c8e1da85fce3df60453dda875
billywiltshire77 <billywiltshire77@gmail.com>
2022-03-22 18:55:30 -0500

    Added Mars Facts Table scrape and table HTML export

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017"
 mongo = PyMongo(app)
+
+ # mars.update_one({}, {"$set": mars_data}, upsert=True) From homework solution, will be helpful
+
+if __name__ == "__main__":
+    app.run(debug=True)
